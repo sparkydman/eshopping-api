7d04fb97ff9031c91ca938407d7f9f435cf5aa5b
Chigozie Ugwuede <sparkyglobe@gmail.com>
2022-12-13 15:13:06 +0100

    created the user model

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,5 @@
 export default {
   port: 1337,
   dbURI: 'mongodb://localhost:27017/rest-post',
+  bcryptSaltRound: 10
 };
---
+++ src/model/user.model.ts
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import config from 'config';
+
+interface IUser {
+  email: string;
+  name: string;
+  password: string;
+}
+
+interface UserDocument extends IUser, mongoose.Document {
+  createdAt: string;
+  updatedAt: string;
+  comparePassword(userPassword: string): Promise<boolean>;
+}
+
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre('save', async function (next) {
+  let user = this as UserDocument;
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(config.get<number>('bcryptSaltRound'));
+  const hashPassword = await bcrypt.hashSync(user.password, salt);
+
+  user.password = hashPassword;
+  return next();
+});
+
+userSchema.methods.comparePassword = async function (
+  userPassword: string
+): Promise<boolean> {
+  let user = this as UserDocument;
+
+  return bcrypt.compare(userPassword, user.password).catch(() => false);
+};
+
+const userModel = mongoose.model('User', userSchema);
